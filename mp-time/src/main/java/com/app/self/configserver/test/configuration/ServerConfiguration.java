61710532e1da4d5ab026152219d3746f51bf44c7
Sardorbek Khalimboev <khalimboevsardorbek@gmail.com>
2022-08-19 14:54:25 +0500

    adding cloud config dependency

--- mp-time/src/main/java/com/app/self/configserver/test/configuration/ServerConfiguration.java
+++ mp-time/src/main/java/com/app/self/configserver/test/configuration/ServerConfiguration.java
@@ -7,8 +7,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Setter
-@Getter @ToString
+@Setter @Getter @ToString
 @ConfigurationProperties(prefix = "mp-time")
 public class ServerConfiguration {
 
--- mp-time/src/main/java/com/app/self/configserver/test/dto/Properties.java
+++ mp-time/src/main/java/com/app/self/configserver/test/dto/Properties.java
@@ -4,8 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
-@AllArgsConstructor
+@Getter @Setter @AllArgsConstructor
 public class Properties {
     private String msg;
 }
